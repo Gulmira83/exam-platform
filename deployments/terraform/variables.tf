922500151c4a23cfd13b5b04e761756702d08c57
Merve Gumuscibik <59778896+mrvgms@users.noreply.github.com>
2020-06-26 16:16:48 -0500

    Update variables.tf

--- deployments/terraform/variables.tf
+++ deployments/terraform/variables.tf
@@ -22,4 +22,14 @@ variable "deployment_image" {
   
 }
 
+variable "deployment_image" {
+  default = "fuchicorp.com/isitup"
+}
 
+variable "deployment_environment" {
+  default = "dev"
+}
+
+variable "google_domain_name" {
+  default = "fuchicorp.com"
+}
