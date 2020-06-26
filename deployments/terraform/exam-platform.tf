b1a1e0b3f14022869b685dcaee983806258d25c9
Merve Gumuscibik <59778896+mrvgms@users.noreply.github.com>
2020-06-26 16:15:01 -0500

    Update exam-platform.tf

--- deployments/terraform/exam-platform.tf
+++ deployments/terraform/exam-platform.tf
@@ -2,7 +2,7 @@ module "exam-platform-deploy" {
   source                 = "fuchicorp/chart/helm"
   deployment_name        = "exam-platform"
   deployment_environment = "${var.deployment_environment}"
-  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}.${var.google_domain_name}")}"
   deployment_path        = "exam-platform"
 
   template_custom_vars = {
